01c3eafb169629c7dca22405f70f1dbeee343946
Youri Pols <youri.pols@edu-kw1c.nl>
2020-02-11 12:35:32 +0100

    Template en Opdracht 2.1 toegevoegd

---
+++ Hoofdstuk 2/opdracht2.1.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * User: Youri Pols
+ * Date: 11-2-2020
+ * Time: 12:26 PM
+ * File: index.php
+ */
+?>
+<html>
+    <head>
+        <title>Mij PHP opdrachten</title>
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+    </head>
+    <body>
+        <?php
+            echo("<p> Hello world </p>");
+        ?>
+        <a href="../index.php">Terug</a>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * User: Youri Pols
+ * Date: 11-2-2020
+ * Time: 12:09 PM
+ * File: index.php
+ */
+?>
+<html>
+    <head>
+        <title>Mij PHP opdrachten</title>
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+    </head>
+    <body>
+    <header>
+        <h1><?php echo "Uitwerking van PHP-opdrachten"; ?></h1>
+    </header>
+    <aside>
+        <h2>Menu</h2>
+        <ul>
+            <li>Hoofdstuk 2
+                <ul>
+                    <li>
+                        <a href="Hoofdstuk%202/opdracht2.1.php">Opdracht 2.1</a>
+                    </li>
+                    <li>
+                        <a href="Hoofdstuk%202/opdracht_2-2.php">Opdracht 2.2</a>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    </aside>
+    <main id="wrapper">
+        <h2>Uitwerkingen</h2>
+    </main>
+    </body>
+</html>
