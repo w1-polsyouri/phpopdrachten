171d3f1c38c57c9dc1f0e7fc1af1bb0f233f4ca0
w1-polsyouri <60651982+w1-polsyouri@users.noreply.github.com>
2020-02-20 10:29:37 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,96 @@
+<<<<<<< HEAD
+<?php
+/**
+ * User: Youri Pols
+ * Date: 11-2-2020
+ * Time: 12:09 PM
+ * File: index.php
+ */
+?>
+<html>
+    <head>
+        <title>Mij PHP opdrachten</title>
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+    </head>
+    <body>
+    <?php
+    include("includes/header.php");
+    ?>
+           <aside>
+                <h2>Menu</h2>
+                <ul>
+                    <li>Hoofdstuk 2
+                        <ul>
+                            <li>
+                                <a href="Hoofdstuk%202/opdracht2.1.php">Opdracht 2.1</a>
+                            </li>
+                            <li>
+                                <a href="Hoofdstuk%202/opdracht_2-2.php">Opdracht 2.2</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <br>
+                    <li>Hoofdstuk 3
+                        <ul>
+                            <li>
+                                <a href="Hoofdstuk%203/opdracht3.1.php">Opdracht 3.1</a>
+                            </li>
+                            <li>
+                                <a href="Hoofdstuk%203/opdracht3.2.php">Opdracht 3.2</a>
+                            </li>
+                            <li>
+                                <a href="Hoofdstuk%203/opdracht3.3.php">Opdracht 3.3</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <br>
+                    <li>Hoofdstuk 4
+                        <ul>
+                            <li>
+                                <a href="#">Opdracht 4.1</a>
+                            </li>
+                            <li>
+                                <a href="#">Opdracht 4.2</a>
+                            </li>
+                            <li>
+                                <a href="#">Opdracht 4.3</a>
+                            </li>
+                            <li>
+                                <a href="#">Opdracht 4.3</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <br>
+                    <li>Hoofdstuk 5
+                        <ul>
+                            <li>
+                                <a href="#">Opdracht 5.1</a>
+                            </li>
+                            <li>
+                                <a href="#">Opdracht 5.2</a>
+                            </li>
+                            <li>
+                                <a href="#">Opdracht 5.3</a>
+                            </li>
+                            <li>
+                                <a href="#">Opdracht 5.4</a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </aside>
+
+    <main id="wrapper">
+        <h2>Uitwerkingen</h2>
+    </main>
+
+    <?php
+    include("includes/footer.php");
+    ?>
+
+    </body>
+</html>
+=======
 <?php
 /**
  * User: Youri Pols
@@ -83,3 +176,4 @@
     </main>
     </body>
 </html>
+>>>>>>> origin/master
